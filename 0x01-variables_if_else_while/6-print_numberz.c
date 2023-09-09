af7b40db600452ac683ffebd79b8a731f1c0dbf4
Isaac Oteng <wisebornjnr@gmail.com>
2023-09-08 21:35:54 -0700

    Putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int u;
+
+	for (u = '0' ; u <= '9' ; u++)
+		putchar(u);
+	putchar('\n');
+	return (0);
+}
