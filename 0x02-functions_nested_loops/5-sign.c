627d030cfd1a1b1874548ff8f66e40df817c21f8
Isaac Oteng <wisebornjnr@gmail.com>
2023-09-13 06:11:45 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,17 +10,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar(n);
+		_putchar(89);
 		return (1);
 	}
 	else if (n < 0)
 	{
-		_putchar(n);
+		_putchar(89);
 		return (-1);
 	}
 	else
 	{
-		_putchar(n);
+		_putchar(89);
 		return (0);
 	}
 }
